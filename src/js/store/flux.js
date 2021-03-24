abba2e14a24c0e01ae73c1b97e787a24a9f7cdb8
FedericoBeeche <fbeeche@gmail.com>
2021-03-24 03:01:33 +0000

    actualizacion

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -11,36 +11,52 @@ const getState = ({ getStore, getActions, setStore }) => {
 			// 	getActions().changeColor(0, "green");
 			// },
 
-			getCharacters: () => {
-				const STAR_WARS_API_PEOPLE = "https://www.swapi.tech/api//people/";
+			getCharacters: async () => {
+				const STAR_WARS_API_PEOPLE = "https://www.swapi.tech/api/people/";
 
-				fetch(STAR_WARS_API_PEOPLE, {})
+				await fetch(STAR_WARS_API_PEOPLE, {})
 					.then(res => {
 						return res.json();
 					})
 					.then(data => {
 						console.log(`Lista de personajes ${data}`);
-						console.table(data);
+						console.table(dataCharacters);
 					})
 					.catch(err => {
 						console.error(`Errors: ${err}`);
 					});
 			},
 
-			//esta es un ejemplo del boilerplate
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			getPlanets: async () => {
+				const STAR_WARS_API_PLANETS = "https://www.swapi.tech/api/planets/";
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+				await fetch(STAR_WARS_API_PLANETS, {})
+					.then(res => {
+						return res.json();
+					})
+					.then(data => {
+						console.log(`Lista de planetas ${data}`);
+						console.table(dataPlanets);
+					})
+					.catch(err => {
+						console.error(`Errors: ${err}`);
+					});
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
+			// //esta es un ejemplo del boilerplate
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
+
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
 			}
 		}
 	};
