37de53a5b0d9a6b877a471fdb4fc390f081e16ff
FedericoBeeche <fbeeche@gmail.com>
2021-03-26 00:25:23 +0000

    actualizacion

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,20 +1,23 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
 import "../../styles/home.scss";
+import { Context } from "../store/appContext";
 
 export const Home = () => {
+	const { store, actions } = useContext(Context);
+
 	return (
-		<div className="card" style={{ width: 18 + "rem" }}>
-			<img src="..." className="card-img-top" alt="..." />
-			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make up the bulk of the cards content.
-				</p>
-				<a href="#" className="btn btn-primary">
-					Learn more!
-				</a>
+		<>
+			<div className="card" style={{ width: 18 + "rem" }}>
+				<img src="..." className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Card Text</p>
+					<a href="#" className="btn btn-primary">
+						Learn more!
+					</a>
+				</div>
 			</div>
-		</div>
+			;
+		</>
 	);
 };
